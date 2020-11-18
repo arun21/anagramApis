14b14ac21c8218e373e11f1435208f71909ec6b8
arun.prabhu <arun.prabhu@mobilelive.ca>
2020-11-18 10:04:23 -0500

    small changes

--- routes/run.py
+++ routes/run.py
@@ -1,4 +1,4 @@
-from application import application
+from server import application
 from flask import Flask, request, abort, Response, jsonify
 from flask_cors import CORS
 from flask_bcrypt import Bcrypt
